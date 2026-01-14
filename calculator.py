6ffdc6084c9f3f68d4ed2e5f8bd1260c3b603646
[naveenumarp2023a-cell] <[naveenkumar.p2023a@vitstudent.ac.in]>
2026-01-14 12:08:30 +0530

    Added functions for subraction,multiplication and division

--- calculator.py
+++ calculator.py
@@ -1,6 +1,17 @@
-def add(a,b):
+def addtion(a,b):
     return a+b
-num1=int(input("Enter number 1: "))
-num2=int(input("Enter number 2: "))
 
-print("Addition of two numbers: ",num1+num2)
+def subraction(a,b):
+    return a-b
+
+def multipication(a,b):
+    return a*b
+
+def division(a,b):
+    return a/b
+
+number1=int(input("Enter number 1: "))
+number2=int(input("Enter number 2: "))
+
+
+print("Addition of two numbers: ",number2+number2)
