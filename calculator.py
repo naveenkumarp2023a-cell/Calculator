864417e87021849f9ef7b46b719cea2ef58f4d3d
[naveenumarp2023a-cell] <[naveenkumar.p2023a@vitstudent.ac.in]>
2026-01-14 12:29:01 +0530

    Added option for users to choose the option

--- calculator.py
+++ calculator.py
@@ -1,10 +1,11 @@
-def addtion(a,b):
+print("Welcome to our Calculator")
+def addition(a,b):
     return a+b
 
 def subraction(a,b):
     return a-b
 
-def multipication(a,b):
+def multiplication(a,b):
     return a*b
 
 def division(a,b):
@@ -13,5 +14,22 @@ def division(a,b):
 number1=int(input("Enter number 1: "))
 number2=int(input("Enter number 2: "))
 
+while True:
+    print("---------A simple calculator---------\n 1. Addition \n 2.Subraction \n 3.Multiplication \n 4.Division \n 5.Exit ")
+    option=int(input("Enter options for operation: "))
 
-print("Addition of two numbers: ",number2+number2)
+    if(option==1):
+        addition(number1,number2)
+        print("Addition of two numbers: ",number1+number2)
+    elif(option==2):
+        subraction(number1,number2)
+        print("Subraction of two numbers: ",number2+number2)
+    elif(option==3):
+        multiplication(number1,number2)
+        print("Multiplication of two numbers: ",number2+number2)
+    elif(option==4):
+        division(number1,number2)
+        print("Division of two numbers: ",number2+number2)
+    elif(option==5):
+        break
+print("Thank you for using  our calculator")
